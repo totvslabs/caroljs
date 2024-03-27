c9dc14f5c105d86b6eb315c81f51c8fc8f672533
Ingo <ingo.wagner@totvs.com>
2024-03-27 09:55:35 -0400

    feat: about endpoint

--- qa-webhook/src/routes/qa-webhook-route.js
+++ qa-webhook/src/routes/qa-webhook-route.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express();
 const qaWebhookController = require("../controllers/qa-webhook-controller");
 
+router.get('/about', (req, res) => {
+    res.json({
+        version: '1.0.0',
+    });
+});
 router.post('/', qaWebhookController.message);
 router.get('/', qaWebhookController.list);
 router.get('/paginated', qaWebhookController.listPaginated);
